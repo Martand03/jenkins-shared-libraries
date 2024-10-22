d6a6f9a329a49cd8770042dfee89b2c20ba741ef
Martand Mahajan <120596767+Martand03@users.noreply.github.com>
2024-10-22 19:52:43 +0530

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,4 +1,4 @@
 def call(String ImageName, String ImageTag){
   echo "This is building the code"
-  bat "docker build -t "${ImageName}":"${ImageTag}" ."
+  bat "docker build -t ${ImageName}:${ImageTag} ."
 }
